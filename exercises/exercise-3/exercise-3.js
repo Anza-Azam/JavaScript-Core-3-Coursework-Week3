af86732bb86474bacdbd64f3d900f99726fa761f
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-09-20 19:24:54 +0100

    exercise 3

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,45 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+const [a, b, c, d, e, f] = order;
+
+const orders = [a, b, c, d, e, f];
+
+const itemN = orders.map((ele) => ele.itemName);
+const quantity = orders.map((elem) => elem.quantity);
+const price = orders.map((el) => el.unitPrice);
+const actualP = orders.map(ele => parseFloat((ele.unitPrice * ele.quantity).toFixed(2)))
+console.log('QTY '+'ITEM    '+'           TOTAL ')
+price.forEach((actualPrice, index) => {
+  if (index === 2) {
+    console.log(
+      quantity[index]+"   "+itemN[index] + "       " + (quantity[index] * actualPrice).toFixed(2)
+    );
+  } else if (index === 3) {
+    console.log(
+      quantity[index] +
+        "   " +
+        itemN[index] +
+        "   " +
+        (quantity[index] * actualPrice).toFixed(2)
+    );
+  } else if (index >3) {
+    console.log(
+      quantity[index] +
+        "   " +
+        itemN[index] +
+        "         " +
+        (quantity[index] * actualPrice).toFixed(2)
+    );
+  } else
+    console.log(
+      quantity[index] +
+        "   " +
+        itemN[index] +
+        "          " +
+        (quantity[index] * actualPrice).toFixed(2)
+    );
+});
+
+console.log('\n'+'Total Price: '+actualP.reduce((totalPrice,curr)=>totalPrice+curr))
